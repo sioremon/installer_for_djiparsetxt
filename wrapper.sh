1f2f039ef31633fe6bb631c28c493666df852064
sioremon <t17651sn@sfc.keio.ac.jp>
2021-06-21 18:31:07 +0900

    hgoe

--- wrapper.sh
+++ wrapper.sh
@@ -5,10 +5,10 @@ mkdir $1/image
 ls $1/DJIFlightRecord_*| while read filepath;do
     filename = basename '$filepath'
     filename = "${filename%.*}"
-    echo $filepath
     echo $filename
-    touch $1/csv/${filename%.*}.csv
-    djiparsetxt $filepath > $1/csv/${filename%.*}.csv
+    touch $1/csv/${filename%.*}csv
+    echo $filepath
+    # djiparsetxt $filepath > $1/csv/${filename%.*}csv
 done
 
 mv *jpg $1/image
