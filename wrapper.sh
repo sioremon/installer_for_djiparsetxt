b11625eadb8342d68cb769085d7eb20d54e93e50
sioremon <t17651sn@sfc.keio.ac.jp>
2021-06-21 18:15:44 +0900

    mendoi

--- wrapper.sh
+++ wrapper.sh
@@ -4,6 +4,8 @@ mkdir $1/image
 ls $1/DJIFlightRecord_*| while read filepath;do
     filename = basename '$filepath'
     filename = "${filename%.*}"
+    echo $filepath
+    echo $filename
     touch $1/csv/${filename%.*}.csv
     djiparsetxt $filepath > $1/csv/${filename%.*}.csv
 done
