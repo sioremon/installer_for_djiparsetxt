6d59b61ef7c8f346453a1ae806074c085013cea8
sioremon <t17651sn@sfc.keio.ac.jp>
2021-06-21 17:49:10 +0900

    mendoi

--- wrapper.sh
+++ wrapper.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 mkdir ./csv
 mkdir ./image
+echo $1
 ls $1 DJIFlightRecord_*| while read filename;do
     touch ./csv/${filename%.*}.csv
     djiparsetxt $filename > ./csv/${filename%.*}.csv
