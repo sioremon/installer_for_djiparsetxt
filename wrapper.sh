6517ef3f5704b24b7b457142c0eb531d3ff750fa
sioremon <t17651sn@sfc.keio.ac.jp>
2021-06-22 15:41:53 +0900

    hoge

--- wrapper.sh
+++ wrapper.sh
@@ -1,14 +1,15 @@
 #/bin/bash
-declare -a filename
-mkdir $1/csv
-mkdir $1/image
-ls $1/DJIFlightRecord_*| while read filepath;do
-    filename = basename '$filepath'
-    filename = "${filename%.*}"
-    echo $filename
-    touch $1/csv/${filename%.*}csv
-    echo $filepath
-    # djiparsetxt $filepath > $1/csv/${filename%.*}csv
-done
 
-mv *jpg $1/image
+for filepath in ${@:2};do
+dir=dirname $filepath
+filename=${filepath##/}
+file=${filename%.}
+csvfile=$dir/$file.csv
+touch $csvfile
+djiparsetxt $filepath >$csvfile
+
+done
+mkdir $dir/image
+mkdir $dir/CSV
+mv $dir/*.csv $dir/CSV
+
